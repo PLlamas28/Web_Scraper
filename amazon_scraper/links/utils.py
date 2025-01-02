93b61321db1bbdc349eade46b89cbd0315fad93f
PLlamas28 <Peter.Llamas@tufts.edu>
2025-01-02 11:07:44 -0500

    Changed utils.py

--- amazon_scraper/links/utils.py
+++ amazon_scraper/links/utils.py
@@ -4,7 +4,7 @@ import requests
 def getInfo(url):
 
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+        "User-Agent": "", # Your info here
         "Accept-Language": "en",
     }
 
